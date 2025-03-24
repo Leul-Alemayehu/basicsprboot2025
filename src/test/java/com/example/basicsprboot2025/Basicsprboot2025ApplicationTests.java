c2d98cbdfb4d78601aa3c3e5a1e8c23b19593269
Leul Alemayehu <leulalemayehu435@gmail.com>
2025-03-24 18:43:46 +0300

    Initial commit

---
+++ src/test/java/com/example/basicsprboot2025/Basicsprboot2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicsprboot2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Basicsprboot2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
